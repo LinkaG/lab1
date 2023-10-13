9ade0391e966c788945f30f7036773ac9196e874
LinkaG <z.gilvanova@yandex.ru>
2023-10-14 02:02:42 +0500

    add log-reg model experment

---
+++ src/model/evaluate.py
@@ -0,0 +1,47 @@
+import joblib
+import click
+from sklearn import metrics
+import pandas as pd
+import json
+from ruamel.yaml import YAML
+import mlflow
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+tracking_uri = os.environ.get("TRACKING_URI")
+
+
+@click.command()
+@click.argument("input_path_data", type=click.Path())
+@click.argument("input_path_label", type=click.Path())
+@click.argument("model_path", type=click.Path())
+@click.argument("output_path", type=click.Path())
+def evaluate(input_path_data: str, input_path_label: str, model_path: str, output_path: str):
+    mlflow.set_tracking_uri(tracking_uri)
+
+    mlflow.set_experiment("logreg classifier")
+    mlflow.start_run(run_name="test_logreg")
+
+    yaml = YAML(typ="safe")
+    params = yaml.load(open("params.yaml", encoding="utf-8"))['model']['params']
+
+    x_test = pd.read_csv(input_path_data)
+    y_test = pd.read_csv(input_path_label)
+    clf = joblib.load(model_path)
+    y_pred = clf.predict(x_test)
+    accuracy = metrics.accuracy_score(y_test, y_pred)
+
+    score = dict(
+        accuracy=float(accuracy)
+    )
+    with open(output_path, 'w') as score_file:
+        json.dump(score, score_file, indent=4)
+
+    mlflow.log_params(params)
+    mlflow.log_metrics(score)
+
+
+if __name__ == "__main__":
+    evaluate()
---
+++ src/model/train.py
@@ -0,0 +1,37 @@
+from hydra.utils import instantiate
+import click
+import pandas as pd
+from joblib import dump
+from ruamel.yaml import YAML
+import mlflow
+import os
+from dotenv import load_dotenv
+
+
+yaml = YAML(typ="safe")
+load_dotenv()
+# tracking_uri = os.environ.get("TRACKING_URI")
+
+
+@click.command()
+@click.argument("input_path_data", type=click.Path())
+@click.argument("input_path_label", type=click.Path())
+@click.argument("output_path", type=click.Path())
+def train(input_path_data: str, input_path_label: str, output_path: str):
+    # mlflow.set_tracking_uri(tracking_uri)
+    mlflow.sklearn.autolog()
+    mlflow.set_experiment("logreg classifier")
+    mlflow.start_run(run_name="train_logreg")
+
+    params = yaml.load(open("params.yaml", encoding="utf-8"))['model']['params']
+
+    x_train = pd.read_csv(input_path_data)
+    y_train = pd.read_csv(input_path_label)
+
+    model = instantiate(params)
+    model.fit(x_train, y_train.values.ravel())
+    dump(model, output_path)
+
+
+if __name__ == "__main__":
+    train()
